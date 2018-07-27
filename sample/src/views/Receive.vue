6d86b66a41b9d847a3a144df0fc1d4a8f4ffd2c1
Dongsoo Lee <dongsoolee8@gmail.com>
2018-07-27 15:06:24 +0900

    Receive 아이템 추가

--- sample/src/views/Receive.vue
+++ sample/src/views/Receive.vue
@@ -2,11 +2,23 @@
   <div class="receive">
     <top-bar title="대금 지급"></top-bar>
     <h1>대금 지급 현황</h1>
+    <block-list :items="receiveItems"></block-list>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Receive'
+  name: 'Receive',
+  data() {
+    return {
+      receiveItems: [
+        { key: 3, blockId: 'AAF14C0', name: '물품3', balance: '1,150,000'},
+        { key: 1, blockId: 'A3F14C0', name: '물품1', balance: '250,000'},
+        { key: 2, blockId: '134C093', name: '물품2', balance: '350,000'},
+        { key: 4, blockId: '0931454', name: '물품4', balance: '0', confirm: true},
+        { key: 5, blockId: '21F1423', name: '물품5', balance: '0', confirm: false}
+      ]
+    }
+  }
 }
 </script>

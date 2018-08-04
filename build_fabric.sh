4b481fad3da96ea0d6fafb3c4e5221b5d10f732b
Dongsoo Lee <dongsoolee8@gmail.com>
2018-08-04 20:33:09 +0900

    Add crypto-config.yaml and fix misspelled org

--- build_fabric.sh
+++ build_fabric.sh
@@ -8,6 +8,32 @@ BINPATH=$(pwd)/bin
 CLIPATH=$PROJPATH/cli/peers
 ORDERERS=$CLIPATH/ordererOrganizations
 PEERS=$CLIPATH/peerOrganizations
+CHANNEL_NAME="default"
 
 rm -rf $CLIPATH
 $BINPATH/cryptogen generate --config=$PROJPATH/crypto-config.yaml --output=$CLIPATH
+
+echo
+echo "##########################################################"
+echo "#########  Generating Orderer Genesis block ##############"
+echo "##########################################################"
+$BINPATH/configtxgen -profile OrgsGenesis -outputBlock $CLIPATH/genesis.block
+
+echo
+echo "#################################################################"
+echo "### Generating channel configuration transaction 'channel.tx' ###"
+echo "#################################################################"
+$BINPATH/configtxgen -profile OrgsChannel -outputCreateChannelTx $CLIPATH/channel.tx -channelID $CHANNEL_NAME
+cp $CLIPATH/channel.tx $PROJPATH/web
+
+echo
+echo "#################################################################"
+echo "####### Generating anchor peer update for InsuranceOrg ##########"
+echo "#################################################################"
+$BINPATH/configtxgen -profile OrgsChannel -outputAnchorPeersUpdate $CLIPATH/CompnayAOrgMSPAnchors.tx -channelID $CHANNEL_NAME -asOrg CompanyAOrgMSP
+
+echo
+echo "#################################################################"
+echo "#######    Generating anchor peer update for ShopOrg   ##########"
+echo "#################################################################"
+$BINPATH/configtxgen -profile OrgsChannel -outputAnchorPeersUpdate $CLIPATH/CompnayBOrgAnchors.tx -channelID $CHANNEL_NAME -asOrg CompanyBOrgMSP
